90f289d4bd200d0c6bece311af9dc83e5bbe9e02
ihlmpy <jsih1236@gmail.com>
2025-01-22 16:27:18 +0100

    Delete, animations, checked

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+interface TodoItem {
+  text: string;
+  checked: boolean;
+}
 
 @Component({
   selector: 'app-todo',
-  imports: [],
+  imports: [ CommonModule ],
+  standalone: true,
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.css'
 })
 
 export class TodoComponent {
-  
+    @ViewChild('todoItemsElement') todoItemsElement?: ElementRef<HTMLDivElement>;
+
+    isMenuVisible = false;
+    // максимум 10 элементов сделать
+    todoItems: TodoItem[] = [
+      { text: "hmmmRIR", checked: false },
+      { text: "hmmmKOK", checked: false },
+      { text: "hmmmRÖR", checked: false },
+      { text: "hmmmLAL", checked: false },
+      { text: "hmmmRIR", checked: false },
+      { text: "hmmmKOK", checked: false },
+      { text: "hmmmRÖR", checked: false },
+      { text: "hmmmLAL", checked: false },
+      { text: "hmmmRIR", checked: false },
+      { text: "hmmmKOK", checked: false },
+    ];
+
+    async changeMenuState() {
+      this.isMenuVisible = !this.isMenuVisible;
+
+      if (this.isMenuVisible) {
+        setTimeout(() => {
+          if (this.todoItemsElement) {
+            this.todoItemsElement.nativeElement.style.zIndex = "0";
+          }
+        }, 1000);
+      }
+    }
+
+    changeChackedStatus(i: number) {
+        let newTodoItems: TodoItem[] = [];
+
+        this.todoItems.forEach((todoItem: TodoItem, idx: number) => {
+           if(idx === i) {
+              newTodoItems.push({ ...todoItem, checked: !todoItem.checked });
+           } else {
+              newTodoItems.push(todoItem);
+           }
+        });
+
+        this.todoItems = newTodoItems;
+    } 
+
+    // сделать анимацию при удалении(в чатгпт помощь маленькая)
+    deleteItem(i: number) {
+      this.todoItems = this.todoItems.filter((item: TodoItem, idx: number) => idx !== i);
+    }
 }
