1e4a8d487d1d8bce0f4c4db3d0c5ccadcd35eb7f
ihlmpy <jsih1236@gmail.com>
2025-01-24 11:09:34 +0100

    Validation for todoinput

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { TodoComponent } from './todo/todo.component';
+import { RouterOutlet } from '@angular/router';     
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    TodoComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'  
--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { trigger, transition, state, style, animate } from '@angular/animations';
-import { FormsModule } from "@angular/forms";
+import { FormBuilder, FormGroup, FormsModule, Validators, ReactiveFormsModule } from "@angular/forms";
 
 interface TodoItem {
   text: string;
@@ -9,7 +9,7 @@ interface TodoItem {
 }
 
 interface EditingItemInputValueInterface {
-  value: string;
+  value: string | null;
   i: number | null;
 }
 
@@ -27,7 +27,8 @@ const Notifications = [
   selector: 'app-todo',
   imports: [ 
     CommonModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   standalone: true,
   animations: [
@@ -43,16 +44,30 @@ const Notifications = [
 
 export class TodoComponent {
     @ViewChild('todoItemsElement') todoItemsElement?: ElementRef<HTMLDivElement>;
-    todoValue: string = "";
+   // todoValue: string = "";
     isMenuVisible = false;
 
     removingItem: TodoItem | null = null;
     editingItem: TodoItem | null = null;
-    editingItemInputValue: EditingItemInputValueInterface = { value: "", i: null };
+    editingItemInputValue: EditingItemInputValueInterface = { value: null, i: null };
+    showNotification: boolean = false;
 
     notification: NotificationInterface = { text: "", isVisible: false };
 
     todoItems: TodoItem[] = [];
+
+    todoValueForm: FormGroup;
+
+    isAllowedNewNotificationTimeout: boolean = true;
+    
+    constructor(private fb: FormBuilder) {
+      this.todoValueForm = this.fb.group({
+        todoValue: [
+          '',
+          [ Validators.required, Validators.minLength(5), Validators.maxLength(199) ]
+        ]
+      })
+    }
     
     // сделать так что бы меню закрывалось когда уже нету элементов и открывалось когда появились
     // сделать так что бы и при нажатии на стрелочку тоже менялось положение меню
@@ -96,20 +111,35 @@ export class TodoComponent {
       }, 4000);
     }
 
+    NotificationTimeout() {
+      if (this.isAllowedNewNotificationTimeout) {
+        this.isAllowedNewNotificationTimeout = false;
+        
+        setTimeout(() => {
+          this.showNotification = false;
+          this.isAllowedNewNotificationTimeout = true;
+        }, 6000);
+      }
+    }
 
-    // переписать систему уведомлений что бы те подходили для каждого вида ошибки
+
     onEnter(event: KeyboardEvent) {
-      if (event.key === "Enter" && this.editingItemInputValue.value === "") {
+      if (event.key === "Enter" && this.editingItemInputValue.value === null) {
+        this.showNotification = true;
+
         if (this.todoItems.length < 10) {
-          if (this.todoValue.length > 200 || this.todoValue.length < 5) {
-            this.UseNotification(0);
-            return;
+          const text = this.todoValueForm.get('todoValue')?.value; 
+
+          if (!this.todoValueForm.get('todoValue')?.errors) {
+            this.todoItems = [{ text, checked: false }, ...this.todoItems];  
+
+            this.todoValueForm.reset();
           }
 
-          this.todoItems = [{ text: this.todoValue, checked: false }, ...this.todoItems];
-          this.todoValue = "";
+          this.NotificationTimeout();
+
         } else {
-          this.todoValue = "";
+          this.todoValueForm.reset();
         }
       }   
     }
@@ -119,16 +149,18 @@ export class TodoComponent {
         this.editingItem = this.todoItems[i];
         this.editingItemInputValue = { value: this.todoItems[i].text, i };
       } else {
-        if (this.editingItemInputValue.value.length < 200 && this.editingItemInputValue.value.length > 5) {
-          this.todoItems = this.todoItems.map((todoItem: TodoItem, idx: number) => {
-            if (idx === i) {
-              return { ...todoItem, text: this.editingItemInputValue.value }
-            }
-  
-            return todoItem;
-          });
+          if (this.editingItemInputValue.value) {
+            if (this.editingItemInputValue.value.length < 200 && this.editingItemInputValue.value.length > 5) {
+              this.todoItems = this.todoItems.map((todoItem: TodoItem, idx: number) => {
+                if (idx === i) {
+                  return { ...todoItem, text: this.editingItemInputValue.value ? this.editingItemInputValue.value : "" }
+                }
+      
+                return todoItem;
+              });
+          }
         } else {
-          this.UseNotification(1);
+          //this.UseNotification(1);
         }
 
         this.editingItem = null;
@@ -159,7 +191,7 @@ export class TodoComponent {
     // перенести 200 и 5 в константы
     
     get TruncatedText() {
-      if (this.editingItemInputValue) {
+      if (this.editingItemInputValue && this.editingItemInputValue.value) {
         return this.editingItemInputValue.value.length > 200 ? 
           this.editingItemInputValue.value.slice(0, 200) :
           this.editingItemInputValue.value;
@@ -169,6 +201,7 @@ export class TodoComponent {
     }
 
     get ExcessText() {
-      return this.editingItemInputValue.value.slice(200);
+      const text = this.editingItemInputValue.value ? this.editingItemInputValue.value.slice(200) : "";
+      return text;
     }
 }
