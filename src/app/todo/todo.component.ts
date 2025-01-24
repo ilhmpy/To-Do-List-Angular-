ef77f77acefda483ebec593d00d520e5d586d1ad
ihlmpy <jsih1236@gmail.com>
2025-01-24 13:51:12 +0100

    Notifications for todoInput

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -8,21 +8,6 @@ interface TodoItem {
   checked: boolean;
 }
 
-interface EditingItemInputValueInterface {
-  value: string | null;
-  i: number | null;
-}
-
-interface NotificationInterface {
-  text: string;
-  isVisible: boolean;
-}
-
-const Notifications = [
-  "No more than 10 tasks allowed", 
-  "you wrote more than 200 charactes or less than 5 charactes"
-];
-
 @Component({
   selector: 'app-todo',
   imports: [ 
@@ -44,72 +29,33 @@ const Notifications = [
 
 export class TodoComponent {
     @ViewChild('todoItemsElement') todoItemsElement?: ElementRef<HTMLDivElement>;
-   // todoValue: string = "";
-    isMenuVisible = false;
 
     removingItem: TodoItem | null = null;
-    editingItem: TodoItem | null = null;
-    editingItemInputValue: EditingItemInputValueInterface = { value: null, i: null };
+    editingItemID: number | null = null;
     showNotification: boolean = false;
-
-    notification: NotificationInterface = { text: "", isVisible: false };
+    isAllowedNewNotificationTimeout: boolean = true;
+    todoValueForm: FormGroup;
 
     todoItems: TodoItem[] = [];
 
-    todoValueForm: FormGroup;
-
-    isAllowedNewNotificationTimeout: boolean = true;
-    
     constructor(private fb: FormBuilder) {
       this.todoValueForm = this.fb.group({
         todoValue: [
           '',
           [ Validators.required, Validators.minLength(5), Validators.maxLength(199) ]
+        ],
+
+        editingItemValue: [
+          null,
+          [ Validators.required, Validators.minLength(5), Validators.maxLength(199) ]
         ]
-      })
-    }
-    
-    // сделать так что бы меню закрывалось когда уже нету элементов и открывалось когда появились
-    // сделать так что бы и при нажатии на стрелочку тоже менялось положение меню
-    // скорее всего проблема в реализации с isMenuVisible && todoItems.length > 0 || todoItems.length > 0
-    // разобраться в чем причина и сделать рефакторинг для исправления 
-
-    changeMenuState() {
-      this.isMenuVisible = !this.isMenuVisible;
+      });
     }
 
-    changeCheckedStatus(i: number) {
-        const sound = new Audio('/sounds/checkboxsound.mp3');
-        sound.play();
-
-        this.todoItems = this.todoItems.map((todoItem: TodoItem, idx: number) => {
-          if (idx === i) {
-            return { ...todoItem, checked: !todoItem.checked}
-          }
-
-          return todoItem;
-        })
-    } 
-    
-    deleteItem(i: number) {
-      if (this.todoItems[i].checked) {
-        const sound = new Audio('/sounds/tothrashsound.mp3');
-        sound.play();
-        this.removingItem = this.todoItems[i];
-  
-        setTimeout(() => {
-          this.todoItems = this.todoItems.filter((item: TodoItem, idx: number) => idx !== i);
-        }, 500);
-      }
-    }
-
-    UseNotification(notificationsType: number) {
-      this.notification = { text: Notifications[notificationsType], isVisible: true };
-
-      setTimeout(() => {
-        this.notification = { ...this.notification, isVisible: false };
-      }, 4000);
-    }
+    // проработать появление уведомления о пустом поле, 
+    // что бы уведомление не показывалось сразу же после очистки поля так как я добавил новый элемент
+    // сделать так что бы когда человек загружался его клавиатура сразу же был сфокусированна на главном инпуте
+    // сделать уведомления также и при редактировании
 
     NotificationTimeout() {
       if (this.isAllowedNewNotificationTimeout) {
@@ -122,9 +68,31 @@ export class TodoComponent {
       }
     }
 
+    changeCheckedStatus(i: number) {
+        new Audio('/sounds/checkboxsound.mp3').play();
+
+        this.todoItems = this.todoItems.map((todoItem: TodoItem, idx: number) => {
+          if (idx === i) {
+            return { ...todoItem, checked: !todoItem.checked}
+          }
+
+          return todoItem;
+        })
+    } 
+    
+    deleteItem(i: number) {
+      if (this.todoItems[i].checked) {
+        new Audio('/sounds/tothrashsound.mp3').play();
+        this.removingItem = this.todoItems[i];
+  
+        setTimeout(() => {
+          this.todoItems = this.todoItems.filter((item: TodoItem, idx: number) => idx !== i);
+        }, 500);
+      }
+    }
 
     onEnter(event: KeyboardEvent) {
-      if (event.key === "Enter" && this.editingItemInputValue.value === null) {
+      if (event.key === "Enter" && this.todoValueForm.get('editingItemValue')?.value === null) {
         this.showNotification = true;
 
         if (this.todoItems.length < 10) {
@@ -145,63 +113,38 @@ export class TodoComponent {
     }
 
     ChangeItemText(i: number) {
-      if (!this.editingItem) {
-        this.editingItem = this.todoItems[i];
-        this.editingItemInputValue = { value: this.todoItems[i].text, i };
+      const valueForm = this.todoValueForm.get('editingItemValue');
+
+      if (valueForm?.value === null) {
+        this.editingItemID = i;
+        valueForm.setValue(this.todoItems[i].text)
       } else {
-          if (this.editingItemInputValue.value) {
-            if (this.editingItemInputValue.value.length < 200 && this.editingItemInputValue.value.length > 5) {
-              this.todoItems = this.todoItems.map((todoItem: TodoItem, idx: number) => {
-                if (idx === i) {
-                  return { ...todoItem, text: this.editingItemInputValue.value ? this.editingItemInputValue.value : "" }
-                }
-      
-                return todoItem;
-              });
-          }
-        } else {
-          //this.UseNotification(1);
+        if (!valueForm?.errors) {
+          this.todoItems = this.todoItems.map((todoItem: TodoItem, idx: number) => {
+            if (idx === i) {
+              return { ...todoItem, text: valueForm?.value ? valueForm.value : "" }
+            }
+
+            return todoItem;
+          });
+
+          this.editingItemID = null;
+          valueForm?.reset();
         }
 
-        this.editingItem = null;
-        this.editingItemInputValue = { value: "", i: null };
+        console.log("ASDSD")
+        this.NotificationTimeout();
       }
     } 
 
     onEdit(i: number) {
-      console.log(this.editingItemInputValue)
       if (
-        this.editingItemInputValue.i === i || 
-        this.editingItemInputValue.i === null
+        this.editingItemID === i || 
+        this.editingItemID === null
       ) {
         this.ChangeItemText(i);
       }
 
       return;
-    }
-
-    onEditingInput(event: KeyboardEvent, i: number) {
-      if (event.key == "Enter") {
-        this.ChangeItemText(i);
-      }
-    }
-    
-    // реализовать подсветку текста красным в случае того если символов больше 200 
-    // с помощью div contentedditable="true"
-    // перенести 200 и 5 в константы
-    
-    get TruncatedText() {
-      if (this.editingItemInputValue && this.editingItemInputValue.value) {
-        return this.editingItemInputValue.value.length > 200 ? 
-          this.editingItemInputValue.value.slice(0, 200) :
-          this.editingItemInputValue.value;
-      } else {
-        return this.editingItemInputValue;
-      }
-    }
-
-    get ExcessText() {
-      const text = this.editingItemInputValue.value ? this.editingItemInputValue.value.slice(200) : "";
-      return text;
     }
 }
